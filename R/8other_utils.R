adffddcf7eee2efd796ff56d4ea8383e1fa3941b
“edo98811” <filippie@uni-mainz.de>
2025-08-04 11:46:31 +0200

    corrected dde wrappers

--- R/8other_utils.R
+++ R/8other_utils.R
@@ -1,12 +1,39 @@
-get_fea_list_wrapper <- function(dde, ...) {
-  tryCatch({return(get_fea_list(dde, ...))},
-  error = {return(NULL)}
+fea_names_wrapper <- function(...) {
+  tryCatch(
+    DeeDeeExperiment::fea_names(...),
+    error = function(e) {
+      warning("get_fea_list_wrapper error: ", conditionMessage(e))
+      NULL
+    }
   )
 }
 
-get_dea_list_wrapper <- function(dde, ...) {
-  tryCatch({return(get_dea_list(dde, ...))},
-  error = {return(NULL)}
+dea_names_wrapper <- function(...) {
+  tryCatch(
+    DeeDeeExperiment::dea_names(...),
+    error = function(e) {
+      warning("get_dea_list_wrapper error: ", conditionMessage(e))
+      NULL
+    }
   )
 }
 
+fea_wrapper <- function(...) {
+  tryCatch(
+    DeeDeeExperiment::fea(...),
+    error = function(e) {
+      warning("fea_wrapper error: ", conditionMessage(e))
+      NULL
+    }
+  )
+}
+
+dea_wrapper <- function(...) {
+  tryCatch(
+    DeeDeeExperiment::dea(...),
+    error = function(e) {
+      warning("dea_wrapper error: ", conditionMessage(e))
+      NULL
+    }
+  )
+}
